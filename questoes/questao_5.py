cffe51274a2c0b9a39a74fc5f961b5ff000c891a
Joaoig14 <36921653+Joaoig14@users.noreply.github.com>
2018-09-05 09:53:40 -0300

    Questão_5.py

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -13,7 +13,13 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 5")
+    qntCigarros = int(input("Qnts cigarros por dia: "))
+anosFumando = int(input("Anos fumando: "))
+
+totalCigarros = (anosFumando * 365)*qntCigarros
+diasPerdidos = (totalCigarros * 10)/24
+
+print ('Dias perdidos %d' %diasPerdidos )
 
 
     
